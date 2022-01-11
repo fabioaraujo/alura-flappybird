aa0895f424bd342b097df0c52605cff179fb6334
Fabio Araujo <fabioaraujods@gmail.com>
2022-01-10 22:53:20 -0300

    incluída movimentação do chão

---
+++ Assets/Scripts/Chao.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade = 0.1f;
+
+    private Vector3 posicaoInicial;
+    private float tamanhoImagem;
+
+    private void Awake()
+    {
+        this.posicaoInicial = this.transform.position;
+        this.tamanhoImagem = this.GetComponent<SpriteRenderer>().size.x * this.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = Mathf.Repeat(this.velocidade * Time.time, this.tamanhoImagem);
+
+        this.transform.position = this.posicaoInicial + (Vector3.left * deslocamento);
+    }
+}
