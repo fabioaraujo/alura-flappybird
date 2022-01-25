2453637869aa6391007e5691da1c1eaca747ddc3
Fabio Araujo <fabioaraujods@gmail.com>
2022-01-24 21:54:16 -0300

    incluida função para mostrar a mensagem game over

--- Assets/Scripts/Diretor.cs
+++ Assets/Scripts/Diretor.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject imagemGameOver;
     public void FinalizarJogo()
     {
         //para o tempo de todo o jogo, paraliza tudo no jogo
         Time.timeScale = 0;
+
+        //habilitar imagem de Game Over
+        this.imagemGameOver.SetActive(true);
     }
 }
